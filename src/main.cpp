e1f8aa572f43f7057c5ea078e003054eb13829da
James Garzón <james@experimentality.co>
2019-02-17 03:30:54 -0500

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Homie.h>
+#include <DHT.h>
+
+const int TEMPERATURE_INTERVAL = 3;
+unsigned long lastTemperatureSent = 0;
+
+const int PIN_DHT22 = 2;    // Broche - Pin DHT22
+DHT dht(PIN_DHT22, DHT22);
+
+HomieNode temperatureNode("temperature", "temperature");
+HomieNode humidityNode("humidity", "humidity");
+
+void loopHandler() {
+  if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL * 1000UL || lastTemperatureSent == 0) {
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+    
+    // Affiche les mesures dans le journal - send values to the logger
+    Homie.getLogger() << "Temperature: " << t << " °C | humidity: " << h << "%" << endl;
+    if ( !isnan(t) ) {
+      temperatureNode.setProperty("temperature").send(String(t));
+      temperatureNode.setProperty("json").send("{\"t\":" + String(t) + ",\"unit\":\"c\"}");
+      humidityNode.setProperty("humidity").send(String(h));
+      humidityNode.setProperty("json").send("{\"h\":" + String(t) + ",\"unit\":\"%\"}");
+      lastTemperatureSent = millis();
+    }
+    lastTemperatureSent = millis();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial << endl << endl;
+
+  Homie_setFirmware("demo-dht22", "1.0.0");
+  Homie.setLoopFunction(loopHandler);
+
+  Homie.setup();
+}
+
+void loop() {
+  Homie.loop();
+}
